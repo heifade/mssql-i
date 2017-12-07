d9a661ec3a0e7a4ebedb71651768546f7321781e
heifade <heifade@126.com>
2017-12-07 18:48:25 +0800

    init

--- es/model/SelectParamsModel.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class SelectParamsModel {
-}
-exports.SelectParamsModel = SelectParamsModel;
-//# sourceMappingURL=SelectParamsModel.js.map
--- es/model/SplitPageResultModel.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class SplitPageResultModel {
-}
-exports.SplitPageResultModel = SplitPageResultModel;
-//# sourceMappingURL=SplitPageResultModel.js.map
