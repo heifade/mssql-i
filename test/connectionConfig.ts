6e12eb607ee3b78e3ffebc26c0e077e67446fa90
heifade <heifade@126.com>
2017-12-07 21:38:42 +0800

    test

--- test/connectionConfig.ts
+++ test/connectionConfig.ts
@@ -1,10 +1,10 @@
 import { config } from "mssql";
 
 export let connectionConfig: config = {
-  server: "127.0.0.1",
+  server: "123.206.217.34",
   user: "travis",
   password: "",
-  database: "mssql-i-test",
+  database: "djd-test",
   port: 1433,
   connectionTimeout: 60000,
   requestTimeout: 60000
