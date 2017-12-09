323a26572d5669a8d0f2b58b3cb84ab0a6a393b3
heifade <heifade@126.com>
2017-12-09 18:26:10 +0800

    edit travis.yml

--- test/connectionConfig.ts
+++ test/connectionConfig.ts
@@ -1,11 +1,21 @@
 import { config } from "mssql";
+import * as program from "commander";
+
+program
+  .version("0.0.1")
+  .option("-p, --ip <n>", "please input sqlserver ip")
+  .option("-u, --user <n>", "please input sqlserver user")
+  .option("--require", "")
+  .parse(process.argv);
 
 export let connectionConfig: config = {
-  server: "123.206.217.34",
-  user: "travis",
+  server: program.ip || '127.0.0.1',
+  user: program.user || 'travis',
   password: "",
   database: "djd-test",
   port: 1433,
   connectionTimeout: 60000,
   requestTimeout: 60000
 };
+
+console.log(connectionConfig);
