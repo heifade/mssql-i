dfafa5ab78469ed29a967612913e4aed77fa4de1
heifade <heifade@126.com>
2017-12-09 19:53:32 +0800

    edit config

--- test/connectionConfig.ts
+++ test/connectionConfig.ts
@@ -4,25 +4,21 @@ import * as program from "commander";
 
 program
   .option("--server <n>", "please input sqlserver ip")
-  .option("--user <n>", "please input sqlserver user")
-  .option("--database <n>", "please input sqlserver database")
   .option("--require", "")
   .parse(process.argv);
 
-if (!program.server || !program.user || !program.database) {
+if (!program.server) {
   let config = JSON.parse(
     fs.readFileSync("./test/config.json", { encoding: "utf-8" })
   );
   program.server = config.server;
-  program.user = config.user;
-  program.database = config.database;
 }
 
 export let connectionConfig: config = {
-  server: program.server,
-  user: program.user,
+  server: program.server || ".",
+  user: "travis",
   password: "",
-  database: program.database,
+  database: "djd-test",
   port: 1433,
   connectionTimeout: 60000,
   requestTimeout: 60000
