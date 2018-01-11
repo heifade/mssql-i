ed895bebffcdac8a901ef534f3408210c017d36e
heifade <heifade@126.com>
2018-01-11 14:46:46 +0800

    2.0.18

--- project-config.ts
+++ project-config.ts
@@ -1,11 +1,18 @@
-import { IProjectConfig } from './src/model/IProjectConfig';
+import { ProjectConfigModel } from "pcreate-config";
 
-
-let projectConfig: IProjectConfig = {
-  projectType: 'node',
+let projectConfig: ProjectConfigModel = {
+  projectType: "node",
+  compile: {
+    outDir: "./es/",
+    module: "commonjs",
+    target: "es5",
+    lib: ["es2015", "es2015.promise", "es2015.symbol"],
+    declaration: true
+  },
   command: false,
   documents: true,
   unitTest: true,
-}
+  sourceInclude: ["./src/**/*"]
+};
 
 export default projectConfig;
